fae23bc59b9333f514a4a10e2fe4a2cf7bc8a358
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-02-25 17:36:45 +0200

    Add implementation of logs for different levels

---
+++ logger.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const reset = '\x1b[0m';
+
+const codes = {
+  info: '\x1b[1;37m',
+  error: '\x1b[1;31m',
+  success: '\x1b[1;32;3m',
+  warning: '\x1b[1;33m',
+};
+
+const makeLogger = (level = 'info') => (string) => {
+  const message = codes[level] + string + reset;
+  console.log(message);
+};
+
+module.exports = {
+  info: makeLogger('info'),
+  error: makeLogger('error'),
+  success: makeLogger('success'),
+  warning: makeLogger('warning'),
+};
