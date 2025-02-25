cfb2d49dee1ef7dadb7e51f21993e23e6215e592
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-02-25 17:19:09 +0200

    add description for functions

--- automated.js
+++ automated.js
@@ -1,3 +1,4 @@
 'use strict';
 
+// Reads coefficients a, b and c from file, validates and returns the array
 module.exports = async (filename) => void console.log('Atomated');
--- interactive.js
+++ interactive.js
@@ -1,3 +1,4 @@
 'use strict';
 
+// Reads coefficients a, b and c from stdin, validates and returns the array
 module.exports = async () => void console.log('Interactive');
