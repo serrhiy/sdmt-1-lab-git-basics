7132f27fc534da929e4327c06c057cf424f67252
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-02-25 17:24:46 +0200

    Add interactive mode
    The user runs the program without additional arguments,
    after which the program reads three coefficients,
    by which it calculates the result

--- interactive.js
+++ interactive.js
@@ -1,4 +1,36 @@
 'use strict';
 
+const process = require('node:process');
+const { isNumeric } = require('./validators.js');
+const readline = require('node:readline/promises');
+const logger = require('./logger.js');
+
+const prompts = ['a = ', 'b = ', 'c = '];
+
+const forever = (method) => {
+  return method().catch((reason) => {
+    throw new Error('Cannot read from the terminal', { cause: reason });
+  }).then((string) => {
+    if (isNumeric(string)) return Number.parseFloat(string);
+    throw new Error(`Coefficient is not a number: ${string}`);
+  }).catch((reason) => {
+    logger.error(reason.message);
+    return forever(method);
+  });
+};
+
+const interactive = async (rl) => {
+  const coefficients = [];
+  for (const prompt of prompts) {
+    const coefficient = await forever(rl.question.bind(rl, prompt));
+    coefficients.push(coefficient);
+  }
+  return coefficients;
+};
+
 // Reads coefficients a, b and c from stdin, validates and returns the array
-module.exports = async () => void console.log('Interactive');
+module.exports = () => {
+  const options = { input: process.stdin, output: process.stdout };
+  const rl = readline.createInterface(options);
+  return interactive(rl).finally(rl.close.bind(rl));
+};
